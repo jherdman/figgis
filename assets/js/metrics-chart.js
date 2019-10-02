94371d613b12367e647b5783a77b62b3e437d007
James Herdman <james.herdman@gmail.com>
2019-10-02 01:21:17 -0400

    WIP

--- assets/js/metrics-chart.js
+++ assets/js/metrics-chart.js
@@ -3,65 +3,122 @@ import 'chartjs-adapter-date-fns';
 
 import socket from './socket';
 
-const metricId = document.location.pathname.split('/').pop();
-
 window.addEventListener('unload', function () {
   socket.disconnect();
 });
 
-const ctx = document.querySelector('.js-chart');
+class MetricsPage {
+  static init () {
+    let page = new this.prototype.constructor();
 
-const chart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    datasets: [
-      {
-        data: [],
-        backgroundColor: '#e9d8fd',
-        borderColor: '#6b46c1',
-        label: 'Change over time',
+    if (page.chartTag) {
+      page.setUpChart();
+      page.setUpChannel();
+    }
+
+    return page;
+  }
+
+  get metricId () {
+    return document.location.pathname.split('/').pop();
+  }
+
+  get chartTag () {
+    return document.querySelector('.js-chart');
+  }
+
+  get spinner () {
+    return document.querySelector('.js-spinner');
+  }
+
+  showData () {
+    document.querySelector('.js-no-data-section').classList.add('hidden');
+    document.querySelector('.js-chart-section').classList.remove('hidden');
+  }
+
+  showInstallInstructions () {
+    document.querySelector('.js-no-data-section').classList.remove('hidden');
+  }
+
+  removeSpinner () {
+    this.spinner.remove();
+  }
+
+  setUpChannel () {
+    this.channel = socket.channel(`metric:${this.metricId}`, {});
+    this.channel.join();
+
+    this.channel.on('data', ({ data }) => {
+      this.removeSpinner();
+
+      if (data.length) {
+        this.showData();
+
+        this.chart.update(data);
+      } else {
+        this.showInstallInstructions();
+      }
+    });
+
+    this.channel.on('new_data', (datum) => {
+      this.showData();
+
+      this.chart.update(Array(datum));
+    });
+  }
+
+  setUpChart () {
+    this.chart = new MetricsChart(this.metricId, this.chartTag);
+  }
+}
+
+class MetricsChart {
+  constructor(id, ctx) {
+    this.id = id;
+
+    this.chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        datasets: [
+          {
+            data: [],
+            backgroundColor: '#e9d8fd',
+            borderColor: '#6b46c1',
+            label: 'Change over time',
+          },
+        ],
       },
-    ],
-  },
-  options: {
-    title: {
-      display: false,
-    },
-    scales: {
-      xAxes: [{
-        type: 'time',
-        time: {
-          unit: 'day',
+      options: {
+        title: {
+          display: false,
         },
-      }],
-    },
-  },
-});
+        scales: {
+          xAxes: [{
+            type: 'time',
+            time: {
+              unit: 'day',
+            },
+          }],
+        },
+      },
+    });
+  }
 
-const channel = socket.channel(`metric:${metricId}`, {});
+  normalizeData({ xValue, yValue }) {
+    return { x: xValue, y: yValue };
+  }
 
-channel.join();
+  update(data) {
+    let normalizedData = data.map(this.normalizeData);
 
-channel.on('data', function ({ data }) {
-  console.log('data received:', data);
-  updateChart(chart, data);
-});
+    this.chart.data.datasets.forEach(function (dataset) {
+      dataset.data.push(...normalizedData);
+    });
 
-channel.on('new_data', function (datum) {
-  console.log('new data receieved:', datum);
-  updateChart(chart, Array(datum));
-});
-
-function normalizeData({ xValue, yValue }) {
-  return { x: xValue, y: yValue };
+    this.chart.update();
+  }
 }
 
-function updateChart(chart, data) {
-  let normalizedData = data.map(normalizeData);
-
-  chart.data.datasets.forEach(function (dataset) {
-    dataset.data.push(...normalizedData);
-  });
-
-  chart.update();
-}
+document.addEventListener('DOMContentLoaded', function () {
+  MetricsPage.init();
+});
--- assets/scripts/publish-test-data.js
+++ assets/scripts/publish-test-data.js
@@ -2,32 +2,14 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 const fetch = require('node-fetch');
-const { addDays } = require('date-fns');
+const addDays = require('date-fns').addDays;
 
+const BASE_DATE = new Date();
 const METRIC_ID = process.env.METRIC_ID;
 const URL = `https://localhost:4000/api/metrics/${METRIC_ID}/data`;
-const BASE_DATE = new Date();
+const LIMIT = parseInt(process.env.LIMIT);
 
-async function doRequest(body) {
-  let resp = await fetch(URL, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/vnd.api+json',
-      'Content-Type': 'application/vnd.api+json',
-    },
-    body: JSON.stringify(body),
-  });
-
-  let json = await resp.json();
-
-  if (resp.ok) {
-    console.log('Created record', json.data.id);
-  } else {
-    console.error('ERROR:', json);
-  }
-}
-
-for (let i = 0; i < 10; i++) {
+async function doRequest (i) {
   const xValue = addDays(BASE_DATE, i);
 
   const yValue = Math.floor(Math.random() * 10000).toString();
@@ -42,5 +24,42 @@ for (let i = 0; i < 10; i++) {
     },
   };
 
-  doRequest(body);
+  const resp = await fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/vnd.api+json',
+      'Content-Type': 'application/vnd.api+json',
+    },
+    body: JSON.stringify(body),
+  });
+
+  const json = await resp.json();
+
+  if (resp.ok) {
+    console.log('Created record', json.data.id);
+  } else {
+    console.error('ERROR:', json);
+  }
 }
+
+async function delay (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function runLoop () {
+  let i = 0;
+
+  while (true) {
+    if (i === LIMIT) {
+      break;
+    }
+
+    await doRequest(i);
+
+    await delay(400);
+
+    i++;
+  }
+}
+
+runLoop();
